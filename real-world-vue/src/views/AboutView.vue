a9f404e962e1b6a681eb80b2c41f7de6b3e5e98e
Maria Eduarda Nichelle <madunichelle@gmail.com>
2024-08-02 23:43:56 -0300

    Add files via upload

--- real-world-vue/src/views/AboutView.vue
+++ real-world-vue/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>A site for events to better the world.</h1>
   </div>
 </template>
 
@@ -12,5 +12,5 @@
     align-items: center;
   }
 } */
+ 
 </style>
-
